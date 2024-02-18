3c295b875fa5dee848fc5b21b2ecceaace017087
Christian Nuss <christian.nuss@gmail.com>
2024-02-18 15:47:12 -0500

    initial project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# serverless-tsoa
+
+⚠️ THIS PLUGIN IS IN DEVELOPMENT ⚠️
+
+This plugin is a wrapper around @tsoa/cli to generate OpenAPI Spec and Routes using tsoa
---
+++ src/index.ts
@@ -0,0 +1,136 @@
+import { generateSpecAndRoutes } from "@tsoa/cli";
+
+type PluginName = "tsoa";
+const PLUGIN_NAME: PluginName = "tsoa";
+
+type PluginConfig = {
+  reloadHandler?: boolean;
+};
+
+type ServerlessCustom = {
+  tsoa?: PluginConfig;
+};
+
+type ServerlessService = {
+  service: string;
+  custom?: ServerlessCustom;
+  provider: {
+    stage: string;
+    environment?: { [key: string]: string | { Ref?: string } };
+  };
+  getAllFunctions: () => string[];
+  getFunction: (functionName: string) => {
+    name: string;
+    events?: any[];
+  };
+};
+
+type ServerlessConfig = {
+  servicePath: string;
+};
+
+type Serverless = {
+  service: ServerlessService;
+  pluginManager: {
+    spawn: (command: string) => Promise<void>;
+  };
+  config: any;
+};
+
+type Options = {
+  verbose?: boolean;
+  log?: ServerlessLog;
+};
+
+type ServerlessLog = ((message: string) => void) & {
+  verbose: (message: string) => void;
+  warning: (message: string) => void;
+  error: (message: string) => void;
+};
+
+class Log {
+  constructor(private options: Options) {}
+
+  static msg = (message: string) => {
+    return `[${PLUGIN_NAME}] ${message}`;
+  };
+
+  log = (message: string) => {
+    if (this.options.log) {
+      this.options.log(Log.msg(message));
+    } else {
+      console.log(Log.msg(message));
+    }
+  };
+
+  verbose = (message: string) => {
+    if (this.options.log) {
+      this.options.log.verbose(Log.msg(message));
+    } else {
+      console.log(Log.msg(message));
+    }
+  };
+
+  warning = (message: string) => {
+    if (this.options.log) {
+      this.options.log.warning(Log.msg(message));
+    } else {
+      console.warn(Log.msg(message));
+    }
+  };
+
+  error = (message: string) => {
+    if (this.options.log) {
+      this.options.log.error(Log.msg(message));
+    } else {
+      console.error(Log.msg(message));
+    }
+  };
+}
+
+class ServerlessTsoa {
+  log: Log;
+
+  serverless: Serverless;
+  serverlessConfig: ServerlessConfig;
+  pluginConfig: PluginConfig;
+
+  hooks: {
+    [key: string]: () => Promise<void>;
+  };
+
+  constructor(serverless: Serverless, protected options: Options) {
+    this.serverless = serverless;
+    this.serverlessConfig = serverless.config;
+    this.pluginConfig =
+      (this.serverless.service.custom &&
+        this.serverless.service.custom[PLUGIN_NAME]) ||
+      {};
+
+    this.log = new Log(options);
+
+    this.hooks = {
+      initialize: async () => {},
+      "before:offline:start": async () => {
+        this.log.verbose("before:offline:start");
+        await this.generateSpecAndRoutes();
+        if (this.pluginConfig.reloadHandler) {
+          await this.watch();
+        }
+      },
+      "before:package:createDeploymentArtifacts": async () => {
+        this.log.verbose("before:package:createDeploymentArtifacts");
+        await this.generateSpecAndRoutes();
+      },
+    };
+  }
+
+  generateSpecAndRoutes = async (): Promise<void> => {
+    const metadata = await generateSpecAndRoutes({});
+    console.log("!!! metadata", metadata);
+  };
+
+  watch = async (): Promise<void> => {};
+}
+
+module.exports = ServerlessTsoa;
