13ac94c9aba40d9dd12b1cf1c30e5177aeb26e67
Christian Nuss <christian.nuss@gmail.com>
2024-02-19 15:31:02 -0500

    improve watching and concurrency

--- src/index.ts
+++ src/index.ts
@@ -97,6 +97,7 @@ class Log {
 
 class ServerlessTsoa {
   log: Log;
+  specHash?: string;
 
   serverless: Serverless;
   serverlessConfig: ServerlessConfig;
@@ -135,19 +136,71 @@ class ServerlessTsoa {
       },
       "before:offline:start": async () => {
         this.log.verbose("before:offline:start");
-        this.generate(this.pluginConfig.reloadHandler).catch(
-          this.handleErrorAndRetry.bind(this)
-        );
+        // await this.generate(); // assuming that tsoa:run was pre-executed
+        await this.watch();
       },
       "before:package:createDeploymentArtifacts": async () => {
         this.log.verbose("before:package:createDeploymentArtifacts");
-        await this.generate(false);
+        await this.generate();
       },
     };
   }
 
-  generate = async (watch?: boolean): Promise<void> => {
-    const { spec, routes, client } = this.pluginConfig;
+  get specFile(): string {
+    const { spec } = this.pluginConfig;
+    if (!spec) {
+      throw new Error(
+        "No custom.tsoa.spec configuration found in serverless.yml"
+      );
+    }
+
+    const specDirectory = spec.outputDirectory;
+
+    return path.join(
+      specDirectory,
+      `${spec.specFileBaseName || "swagger"}.${spec.yaml ? "yaml" : "json"}`
+    );
+  }
+
+  get routesFile(): string {
+    const { routes } = this.pluginConfig;
+    if (!routes) {
+      throw new Error(
+        "No custom.tsoa.routes configuration found in serverless.yml"
+      );
+    }
+
+    const routesDirectory = routes.routesDir;
+
+    return path.join(
+      routesDirectory,
+      `${routes.routesFileName || "routes.ts"}`
+    );
+  }
+
+  get clientFiles(): string[] {
+    const { client } = this.pluginConfig;
+    if (!client) {
+      return [];
+    }
+
+    let clientFiles: string[] = [];
+
+    if (typeof client === "string") {
+      clientFiles.push(client);
+    } else {
+      if (client.target) {
+        clientFiles.push(client.target);
+      }
+    }
+
+    // TODO: Discover more autogen files from orval
+
+    return clientFiles;
+  }
+
+  generate = async (): Promise<void> => {
+    const { spec, routes } = this.pluginConfig;
     if (!spec) {
       throw new Error(
         "No custom.tsoa.spec configuration found in serverless.yml"
@@ -178,108 +231,149 @@ class ServerlessTsoa {
       `.${PLUGIN_NAME}`
     );
 
-    const specDirectory = spec.outputDirectory;
-    const specFile = path.join(
-      specDirectory,
-      `${spec.specFileBaseName || "swagger"}.${spec.yaml ? "yaml" : "json"}`
-    );
-    const workdirSpecFile = path.join(workDir, specFile);
+    const workdirSpecFile = path.join(workDir, this.specFile);
     spec.outputDirectory = path.join(workDir, spec.outputDirectory);
 
-    const routesDirectory = routes.routesDir;
-    const routesFile = path.join(
-      routesDirectory,
-      `${routes.routesFileName || "routes.ts"}`
-    );
+    openApiDestinations.push(this.specFile);
 
-    await generateTsoaSpec(spec);
-    await this.conditionalCopy(workdirSpecFile, specFile);
-    this.log.verbose(`Generated OpenAPI Spec: ${specFile}`);
-
-    await Promise.all(
-      openApiDestinations.map(async (dest) => {
-        await this.conditionalCopy(specFile, path.join(dest, specFile));
-      })
-    );
-
-    await generateTsoaRoutes(routes);
-    this.log.verbose(`Generated OpenAPI Routes: ${routesFile}`);
-
-    let clientFiles: string[] = [];
-    const target = typeof client === "string" ? client : client?.target;
-
-    if (client && target) {
-      const workdirTarget = path.join(workDir, target);
-
-      let output: ClientOutputConfig =
-        typeof client === "string"
-          ? { target: workdirTarget }
-          : { ...client, target: workdirTarget };
-
-      await generateClientSpec(
-        {
-          input: {
-            target: workdirSpecFile,
-          },
-          output,
-        },
-        this.serverlessConfig.servicePath
-      );
-
-      // TODO: Gather additional client files for more advanced configurations
-
-      await this.conditionalCopy(workdirTarget, target);
-      this.log.verbose(`Generated OpenAPI Client: ${target}`);
-      clientFiles.push(target);
-    }
-
-    if (!watch) {
+    // Capture a hash of the spec file, which will be used to determine if a reload is necessary
+    this.specHash = await this.hashFile(workdirSpecFile);
+    try {
+      await generateTsoaSpec(spec);
+    } catch (e) {
+      if (!(e instanceof Error)) {
+        throw e;
+      }
+      this.specHash = undefined;
+      this.log.warning(e.message);
       return;
     }
 
-    let excludeFiles = [specFile, routesFile, ...clientFiles];
+    const newSpecHash = await this.hashFile(workdirSpecFile);
+
+    // Nothing changed, bail early
+    if (this.specHash === newSpecHash) {
+      return;
+    } else {
+      this.log.verbose(`Generated OpenAPI Spec`);
+      this.specHash = newSpecHash;
+    }
+
+    // Using .then becuse the following functions are not dependent on each other
+    generateTsoaRoutes({ ...routes, noWriteIfUnchanged: true })
+      .then(() => {
+        this.log.verbose(`Generated OpenAPI Routes: ${this.routesFile}`);
+      })
+      .catch((e) => {
+        this.log.warning(e.message);
+      });
+
+    this.generateClientSpec(workdirSpecFile, workDir)
+      .then((clientFile) => {
+        if (clientFile) {
+          this.log.verbose(`Generated OpenAPI Client: ${clientFile}`);
+        }
+      })
+      .catch((e) => {
+        this.log.warning(e.message);
+      });
+
+    openApiDestinations.map((destination) =>
+      this.conditionalCopy(
+        workdirSpecFile,
+        path.join(destination, this.specFile)
+      )
+        .then((dest) => {
+          if (dest) {
+            this.log.verbose(`Copied OpenAPI Spec to: ${dest}`);
+          }
+        })
+        .catch((e) => {
+          this.log.warning(e.message);
+        })
+    );
+  };
+
+  generateClientSpec = async (
+    specFile: string,
+    workDir: string
+  ): Promise<string | undefined> => {
+    const { client } = this.pluginConfig;
+    if (!client) {
+      return;
+    }
+
+    const target = typeof client === "string" ? client : client.target;
+    if (!target) {
+      return;
+    }
+
+    const workdirTarget = path.join(workDir, target);
+
+    const output: ClientOutputConfig =
+      typeof client === "string"
+        ? { target: workdirTarget }
+        : { ...client, target: workdirTarget };
+
+    await generateClientSpec(
+      {
+        input: {
+          target: specFile,
+        },
+        output,
+      },
+      this.serverlessConfig.servicePath
+    );
+
+    await this.conditionalCopy(workdirTarget, target);
+
+    return target;
+  };
+
+  watch = async (): Promise<void> => {
+    if (!this.pluginConfig.reloadHandler) {
+      return;
+    }
 
     const watcher = chokidar.watch(
-      path.join(this.serverlessConfig.servicePath),
+      path.join(this.serverlessConfig.servicePath, "src"),
       {
         awaitWriteFinish: true,
         atomic: true,
         ignorePermissionErrors: true,
-        persistent: false,
+        persistent: true,
         ignored: /(^|[\/\\])\../, // ignore dotfiles
       }
     );
 
-    watcher.unwatch(excludeFiles);
+    watcher.unwatch([this.specFile, this.routesFile, ...this.clientFiles]);
 
-    const handler = (file: string) => {
+    watcher.on("change", (file) => {
       watcher.close();
       this.log.verbose(`File ${file} has been changed`);
-      this.generate(this.pluginConfig.reloadHandler).catch(
-        this.handleErrorAndRetry.bind(this)
-      );
-    };
-
-    watcher.on("change", handler);
+      this.generate().then(() => {
+        this.watch();
+      });
+    });
   };
 
-  handleErrorAndRetry = (error: Error) => {
-    this.log.warning(error.message);
-    this.generate(this.pluginConfig.reloadHandler).catch(
-      this.handleErrorAndRetry.bind(this)
-    );
-  };
-
-  conditionalCopy = async (src: string, dest: string): Promise<void> => {
+  conditionalCopy = async (
+    src: string,
+    dest: string
+  ): Promise<string | undefined> => {
     // hash src and dest
-    const srcHash = await this.hashFile(src);
-    const destHash = await this.hashFile(dest);
+    const [srcHash, destHash] = await Promise.all([
+      this.hashFile(src),
+      this.hashFile(dest),
+    ]);
 
-    if (srcHash !== destHash) {
-      await fs.ensureDir(path.dirname(dest));
-      await fs.copy(src, dest);
-      this.log.verbose(`Copied ${src} to ${dest}`);
+    if (srcHash === destHash) {
+      return;
     }
+
+    await fs.ensureDir(path.dirname(dest));
+    await fs.copyFile(src, dest);
+    return dest;
   };
 
   hashFile = async (file: string): Promise<string> => {
@@ -287,6 +381,7 @@ class ServerlessTsoa {
       const buffer = await fs.readFile(file);
       return sha1(buffer);
     } catch (e) {
+      this.log.verbose(`Error hashing file: ${file}`);
       // Return randomness to force a copy
       return sha1(uuid());
     }
